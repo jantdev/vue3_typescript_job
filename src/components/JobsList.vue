062cd278da17ec32377f0bc2ff913414a01a702a
Jan T. Jensen <hireme@jantdev.com>
2022-02-10 22:15:57 +0100

    init

---
+++ src/components/JobsList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="job-list">
+        <ul>
+            <li v-for="job in jobs" :key="job.id">{{job.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,PropType } from 'vue'
+import Job from '@/types/Job'
+export default defineComponent({
+    props:{
+        jobs:{
+            required:true,
+            type:Array as PropType<Job[]>
+        }
+    },
+    setup(props) {
+        console.log(props)
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
